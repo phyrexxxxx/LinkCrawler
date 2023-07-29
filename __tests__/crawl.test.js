dd9d0a7ff86f7877d44ec7ead6cc6cac52ef3523
phyrexxxxx <phyrexxxxx@gmail.com>
2023-07-29 21:30:36 +0800

    feat: implement getURLsFromHTML function and add tests

--- __tests__/crawl.test.js
+++ __tests__/crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals');
-const { normalizeURL } = require('../src/crawl');
+const { normalizeURL, getURLsFromHTML } = require('../src/crawl');
 
 test('normalizeURL strip protocol', () => {
     const input = 'https://wagslane.dev/path';
@@ -19,5 +19,109 @@ test('normalizeURL capitals', () => {
     const input = 'hTtpS://wAgSLane.DEv/path/';
     const actual = normalizeURL(input);
     const expected = 'wagslane.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML absolute path', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://my.website.com/">My Website</a>
+    <body>
+</html>
+`;
+    const inputBaseURL = "https://my.website.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://my.website.com/"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML relative path', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/">My Website Path</a>
+    <body>
+</html>
+`;
+    const inputBaseURL = "https://my.website.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://my.website.com/path/"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML both absolute and relative path', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://my.website.com/path1/">Path1</a>
+        <a href="/path2/">Path2</a>
+    <body>
+</html>
+`;
+    const inputBaseURL = "https://my.website.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [
+        "https://my.website.com/path1/",
+        "https://my.website.com/path2/"
+    ];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML with complex HTML structure', () => {
+    const inputHTMLBody = `
+<html>
+    <head>
+        <!-- comment -->
+        <title>My Website</title>
+    </head>
+    <body>
+        <div>
+            <p>
+                <a href="/path/to/page/">Nested link</a>
+            </p>
+            <span> <a href="https://external.site.com/">External link</a></span>
+        </div>
+        <footer>
+            <a href="/footer/">Footer link</a>
+        </footer>
+    </body>
+</html>
+`;
+    const inputBaseURL = "https://my.website.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [
+        "https://my.website.com/path/to/page/", 
+        "https://external.site.com/",
+        "https://my.website.com/footer/"
+    ];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML with special characters in URL', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/with?query=parameters&more=parameters#fragment">Link with special characters</a>
+    <body>
+</html>
+`;
+    const inputBaseURL = "https://special.characters.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://special.characters.com/path/with?query=parameters&more=parameters#fragment"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML invalid URL', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">Link with special characters</a>
+    <body>
+</html>
+`;
+    const inputBaseURL = "https://my.website.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 })
--- src/crawl.js
+++ src/crawl.js
@@ -1,3 +1,23 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const aElements = dom.window.document.querySelectorAll('a');
+    for (const aElement of aElements) {
+        let href = aElement.href;
+
+        try {
+            // ? relative path : absolute path OR invalid path
+            const urlObj = (href.startsWith('/')) ? new URL(href, baseURL) : new URL(href);
+            urls.push(urlObj.href);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+    return urls;
+}
+
 function normalizeURL(url) {
     if (url.length > 0 && url.slice(-1) === '/') {
         url = url.slice(0, -1);
@@ -7,5 +27,6 @@ function normalizeURL(url) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 };

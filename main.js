d5dc23465d0e925aa6e28c858efccda20ceef845
phyrexxxxx <phyrexxxxx@gmail.com>
2023-07-29 23:19:08 +0800

    feat(main): implement command line argument handling

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+function main() {
+    const args = process.argv.slice(2);
+    if (args.length !== 1) {
+        console.error('Please provide exactly one argument: the baseURL to start the crawl');
+        process.exit(1); // exit with error status code'
+    }
+    const baseURL = args[0];
+    console.log(`Starting the crawler at baseURL: ${baseURL}`);
+}
+
+main();

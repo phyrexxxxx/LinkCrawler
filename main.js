c8530d26096480e433b683662473d66c31ad34ad
phyrexxxxx <phyrexxxxx@gmail.com>
2023-07-30 17:58:14 +0800

    feat: implement recursive web crawling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./src/crawl');
 
-function main() {
+async function main() {
     const args = process.argv.slice(2);
     if (args.length !== 1) {
         console.error('Please provide exactly one argument: the baseURL to start the crawl');
@@ -8,7 +8,11 @@ function main() {
     }
     const baseURL = args[0];
     console.log(`Starting the crawler at baseURL: ${baseURL}`);
-    crawlPage(baseURL);
+    
+    const pages = await crawlPage(baseURL, baseURL, {});
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 
 main();
--- src/crawl.js
+++ src/crawl.js
@@ -1,24 +1,48 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
     try {
+        const baseURLObj = new URL(baseURL);
+        const currentURLObj = new URL(currentURL);
+
+        // We only want to crawl the same domain of baseURL, not the entire internet
+        if (baseURLObj.hostname !== currentURLObj.hostname) {
+            return pages;
+        }
+
+        const currentNormalizeURL = normalizeURL(currentURL);
+        if (pages[currentNormalizeURL] > 0) { // already crawled
+            pages[currentNormalizeURL]++;
+            return pages;
+        }
+        pages[currentNormalizeURL] = 1;
+
+        console.log(`Actively crawling ${currentURL}`);
         const response = await fetch(currentURL);
-        
+
         if (response.status > 399) {
             console.log(`Error in fetch with status code: ${response.status} on page: ${currentURL}`);
-            return;
+            // return;
+            return pages;
         }
         
         const contentType = response.headers.get('Content-Type');
         if (!contentType.includes('text/html')) {
             console.log(`Non-HTML response, content type: ${contentType} on page: ${currentURL}`);
-            return;
+            // return;
+            return pages;
         }
         
-        console.log(await response.text());
+        currentHTMLBody = await response.text();
+        nextURLs = getURLsFromHTML(currentHTMLBody, baseURL);
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages);
+        }
+
     } catch (err) {
         console.log(err.message);
     }
+    return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
